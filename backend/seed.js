00c54c382041751e398b80d1535caf7378850c61
karthick2007-bca <pandi2142007@gmail.com>
2026-03-08 20:52:31 +0530

    Remove test files

--- backend/seed.js
+++ backend/seed.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb//localhost:27017/transitpay')
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.log(err));
+mongoose.connect('mongodb+srv://karthi2142007:Karthick2024@cluster0.nfyak0h.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
 
 const studentSchema = new mongoose.Schema({
   id: String,
