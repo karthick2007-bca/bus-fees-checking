0a39f096ec9087ba857c750bcaca218f95d00c47
karthick2007-bca <pandi2142007@gmail.com>
2026-03-08 15:13:13 +0530

    Fix production API URL

--- backend/public/flutter_service_worker.js
+++ backend/public/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "caaf9c691bd5ca1f214e0ca1171984d8
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "57de1d81b3de93b7ce309579520ce746",
+"flutter_bootstrap.js": "cea6f711e76699739f2c3ab71bb38ad3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
