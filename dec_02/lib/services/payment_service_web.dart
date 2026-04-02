d56d1ce54915a31f7fbcb3f43f95055b010a5c0f
karthick2007-bca <pandi2142007@gmail.com>
2026-04-02 11:28:41 +0530

    Fix allowInterop error in payment_service_web

--- dec_02/lib/services/payment_service_web.dart
+++ dec_02/lib/services/payment_service_web.dart
@@ -1,10 +1,6 @@
-// ignore: avoid_web_libraries_in_flutter
 library payment_service_web;
 
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:js' as js;
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:js' show allowInterop;
+import 'dart:js' show JsObject, context;
 
 class PaymentServiceImpl {
   Function(Map<String, dynamic>)? _onSuccess;
@@ -25,29 +21,28 @@ class PaymentServiceImpl {
     required String phone,
     required String email,
   }) {
-    // Store callbacks in JS context so they can be called from JS
-    js.context['_flutterPaymentSuccess'] = allowInterop((paymentId, orderId, signature) {
-      _onSuccess?.call({
-        'paymentId': paymentId?.toString() ?? '',
-        'orderId': orderId?.toString() ?? '',
-        'signature': signature?.toString() ?? '',
-      });
-    });
-
-    js.context['_flutterPaymentDismiss'] = allowInterop(() {
-      _onFailure?.call({'message': 'Payment cancelled by user'});
-    });
-
-    final options = js.JsObject.jsify({
+    final options = JsObject.jsify({
       'key': 'rzp_live_SNyLCysaEf0ooI',
       'amount': (amount * 100).toInt(),
       'name': 'Fee Payment',
       'description': 'Student Fee Payment',
       'prefill': {'contact': phone, 'email': email},
+      'handler': (response) {
+        _onSuccess?.call({
+          'paymentId': response['razorpay_payment_id'] ?? '',
+          'orderId': response['razorpay_order_id'] ?? '',
+          'signature': response['razorpay_signature'] ?? '',
+        });
+      },
+      'modal': {
+        'ondismiss': () {
+          _onFailure?.call({'message': 'Payment cancelled by user'});
+        }
+      },
       'theme': {'color': '#4F46E5'},
     });
 
-    js.context.callMethod('openRazorpayCheckout', [options]);
+    context.callMethod('openRazorpay', [options]);
   }
 
   void dispose() {}
